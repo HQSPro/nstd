cb3624091448e04e5d1e6bc62ff472c29632e82a
wenjh <zpwenjh@163.com>
2023-07-17 19:10:38 +0800

    match like rust

--- include/match.hpp
+++ include/match.hpp
@@ -2,165 +2,172 @@
 #ifndef __NSTD_MATCH_HPP__
 #define __NSTD_MATCH_HPP__
 
-#include<type_traits>
-#include<type_magic.hpp>
-
+#include <type_traits>
+#include <type_magic.hpp>
+#include <result.hpp>
 namespace nstd {
 
-#define PP_COMMA() ,
-#define PP_LPAREN() (
-#define PP_RPAREN() )
-#define PP_EMPTY()
+/* The first implementations of MATCH like rust.
+ * This approach is implemented through macros which use switch actually.
+ * Note: currently, only 30 branches are supported for matching.
+ */
+#define MATCH_CONCAT_IMPL(A, B) A##B
+#define MATCH_CONCAT(A, B) MATCH_CONCAT_IMPL(A, B)
 
-#define PP_REMOVE_PARENS(T) PP_REMOVE_PARENS_IMPL T
-#define PP_REMOVE_PARENS_IMPL(...) __VA_ARGS__
-
-#define PP_CONCAT(A, B) PP_CONCAT_IMPL(A, B)
-#define PP_CONCAT_IMPL(A, B) A##B
-
-#define PP_INC(N) PP_CONCAT(PP_INC_, N)
-#define PP_INC_0 1
-#define PP_INC_1 2
-// ...
-#define PP_INC_254 255
-#define PP_INC_255 256
-
-#define PP_DEC(N) PP_CONCAT(PP_DEC_, N)
-#define PP_DEC_256 255
-#define PP_DEC_255 254
-// ...
-#define PP_DEC_2 1
-#define PP_DEC_1 0
-
-#define PP_NOT(N) PP_CONCAT(PP_NOT_, N)
-#define PP_NOT_0 1
-#define PP_NOT_1 0
-
-#define PP_AND(A, B) PP_CONCAT(PP_AND_, PP_CONCAT(A, B))
-#define PP_AND_00 0
-#define PP_AND_01 0
-#define PP_AND_10 0
-#define PP_AND_11 1
-
-#define PP_BOOL(N) PP_CONCAT(PP_BOOL_, N)
-#define PP_BOOL_0 0
-#define PP_BOOL_1 1
-#define PP_BOOL_2 1
-// ...
-
-#define PP_IF(PRED, THEN, ELSE) PP_CONCAT(PP_IF_, PP_BOOL(PRED))(THEN, ELSE)
-#define PP_IF_1(THEN, ELSE) THEN
-#define PP_IF_0(THEN, ELSE) ELSE
-
-#define PP_COMMA_IF(N) PP_IF(N, PP_COMMA, PP_EMPTY)()
-
-#define PP_GET_N(N, ...) PP_CONCAT(PP_GET_N_, N)(__VA_ARGS__)
-#define PP_GET_N_0(_0, ...) _0
-#define PP_GET_N_1(_0, _1, ...) _1
-#define PP_GET_N_2(_0, _1, _2, ...) _2
-// ...
-#define PP_GET_N_8(_0, _1, _2, _3, _4, _5, _6, _7, _8, ...) _8
-
-#define PP_GET_TUPLE(N, T) PP_GET_N(N, PP_REMOVE_PARENS(T))
-
-#define PP_IS_EMPTY(...)                                      \
-  PP_AND(PP_AND(PP_NOT(PP_HAS_COMMA(__VA_ARGS__)),            \
-                PP_NOT(PP_HAS_COMMA(__VA_ARGS__()))),         \
-         PP_AND(PP_NOT(PP_HAS_COMMA(PP_COMMA_V __VA_ARGS__)), \
-                PP_HAS_COMMA(PP_COMMA_V __VA_ARGS__())))
-#define PP_HAS_COMMA(...) PP_GET_N_8(__VA_ARGS__, 1, 1, 1, 1, 1, 1, 1, 0, 0)
-#define PP_COMMA_V(...) ,
-
-#define PP_VA_OPT_COMMA(...) PP_COMMA_IF(PP_NOT(PP_IS_EMPTY(__VA_ARGS__)))
+#define MATCH_EXPAND(...) __VA_ARGS__
+#define MATCH_DEFAULT_SYMBOL_
 
 
-#define DO_EACH(VAR, IDX, CTX) PP_COMMA_IF(IDX) CTX VAR
+// #define MATCH_DEFAULT_SYMBOL(PREFIX, SYMBOL) MATCH_CONCAT(PREFIX, SYMBOL)
 
-#define PP_NARG(...)                                                           \
-  PP_GET_N(8, __VA_ARGS__ PP_VA_OPT_COMMA(__VA_ARGS__) 8, 7, 6, 5, 4, 3, 2, 1, \
-           0)
+#define MATCH_PARENS_PROBE(...) _, 1
+#define MATCH_PARENS_CONDITION(a, b, ...) b
+#define MATCH_PARENS_CHECK(...) MATCH_PARENS_CONDITION(__VA_ARGS__, 0)
+#define MATCH_PARENS_REMOVE_IMPL_0(...) __VA_ARGS__
+#define MATCH_PARENS_REMOVE_IMPL_1_IMPL(...) __VA_ARGS__
+#define MATCH_PARENS_REMOVE_IMPL_1(expr) MATCH_PARENS_REMOVE_IMPL_1_IMPL expr
+#define MATCH_PARENS_REMOVE_IMPL(condition) MATCH_CONCAT(MATCH_PARENS_REMOVE_IMPL_, condition)
+#define MATCH_PARENS_REMOVE(expr) \
+    MATCH_PARENS_REMOVE_IMPL(MATCH_PARENS_CHECK(MATCH_PARENS_PROBE expr))(expr)
 
-#define PP_FOR_EACH(DO, CTX, ...) \
-  PP_CONCAT(PP_FOR_EACH_, PP_NARG(__VA_ARGS__))(DO, CTX, 0, __VA_ARGS__)
-#define PP_FOR_EACH_0(DO, CTX, IDX, ...)
-#define PP_FOR_EACH_1(DO, CTX, IDX, VAR, ...) DO(VAR, IDX, CTX)
-#define PP_FOR_EACH_2(DO, CTX, IDX, VAR, ...) \
-  DO(VAR, IDX, CTX)                           \
-  PP_FOR_EACH_1(DO, CTX, PP_INC(IDX), __VA_ARGS__)
-#define PP_FOR_EACH_3(DO, CTX, IDX, VAR, ...) \
-  DO(VAR, IDX, CTX)                           \
-  PP_FOR_EACH_2(DO, CTX, PP_INC(IDX), __VA_ARGS__)
-// ...
+#define MATCH_CASE_PROBE(...) _, 1
+#define MATCH_CASE_CONDITION(a, b, ...) b
+#define MATCH_CASE_CHECK(...) MATCH_CASE_CONDITION(__VA_ARGS__, 0)
+#define MATCH_CASE_PARSE_IMPL_0(...)
+#define MATCH_CASE_DEFAULT_PARSE()
+#define MATCH_CASE_PARSE_IMPL_1_IMPL(m, c, ...) \
+    case c:                                     \
+        return __VA_ARGS__();
+#define MATCH_CASE_PARSE_IMPL_1(m, c) MATCH_CASE_PARSE_IMPL_1_IMPL(m, c)
+#define MATCH_CASE_PARSE_IMPL(condition) MATCH_CONCAT(MATCH_CASE_PARSE_IMPL_, condition)
+#define MATCH_CASE_PARSE(m, c) \
+    MATCH_CASE_PARSE_IMPL(MATCH_CASE_CHECK(MATCH_CASE_PROBE c))(m, MATCH_PARENS_REMOVE(c))
 
+#define MATCH_CASES_PARSE_IMPL(m,   \
+                               c0,  \
+                               c1,  \
+                               c2,  \
+                               c3,  \
+                               c4,  \
+                               c5,  \
+                               c6,  \
+                               c7,  \
+                               c8,  \
+                               c9,  \
+                               c10, \
+                               c11, \
+                               c12, \
+                               c13, \
+                               c14, \
+                               c15, \
+                               c16, \
+                               c17, \
+                               c18, \
+                               c19, \
+                               c20, \
+                               c21, \
+                               c22, \
+                               c23, \
+                               c24, \
+                               c25, \
+                               c26, \
+                               c27, \
+                               c28, \
+                               c29, \
+                               ...) \
+    MATCH_CASE_PARSE(m, c0)         \
+    MATCH_CASE_PARSE(m, c1)         \
+    MATCH_CASE_PARSE(m, c2)         \
+    MATCH_CASE_PARSE(m, c3)         \
+    MATCH_CASE_PARSE(m, c4)         \
+    MATCH_CASE_PARSE(m, c5)         \
+    MATCH_CASE_PARSE(m, c6)         \
+    MATCH_CASE_PARSE(m, c7)         \
+    MATCH_CASE_PARSE(m, c8)         \
+    MATCH_CASE_PARSE(m, c9)         \
+    MATCH_CASE_PARSE(m, c10)        \
+    MATCH_CASE_PARSE(m, c11)        \
+    MATCH_CASE_PARSE(m, c12)        \
+    MATCH_CASE_PARSE(m, c13)        \
+    MATCH_CASE_PARSE(m, c14)        \
+    MATCH_CASE_PARSE(m, c15)        \
+    MATCH_CASE_PARSE(m, c16)        \
+    MATCH_CASE_PARSE(m, c17)        \
+    MATCH_CASE_PARSE(m, c18)        \
+    MATCH_CASE_PARSE(m, c19)        \
+    MATCH_CASE_PARSE(m, c20)        \
+    MATCH_CASE_PARSE(m, c21)        \
+    MATCH_CASE_PARSE(m, c22)        \
+    MATCH_CASE_PARSE(m, c23)        \
+    MATCH_CASE_PARSE(m, c24)        \
+    MATCH_CASE_PARSE(m, c25)        \
+    MATCH_CASE_PARSE(m, c26)        \
+    MATCH_CASE_PARSE(m, c27)        \
+    MATCH_CASE_PARSE(m, c28)        \
+    MATCH_CASE_PARSE(m, c29)
 
+#define MATCH_CASES_PARSE(m, ...) \
+    MATCH_CASES_PARSE_IMPL(       \
+        m, ##__VA_ARGS__, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _, _)
 
-PP_FOR_EACH(DO_EACH, void, )        // (empty)
-PP_FOR_EACH(DO_EACH, int, a, b, c)  // -> int a, int b, int c
-PP_FOR_EACH(DO_EACH, bool, x)       // -> bool x
+#define MATCH_IMPL(matchable, ...)                    \
+    [&matchable]() -> decltype(auto) {                \
+        switch(matchable)                             \
+        {                                             \
+            MATCH_CASES_PARSE(matchable, __VA_ARGS__) \
+        }                                             \
+    }()
 
+#define MATCH(matchable, ...) MATCH_IMPL(MATCH_PARENS_REMOVE(matchable), __VA_ARGS__)
 
-#define MATCH_CASE(I, F) case I: F()
+/* The second implementations of match like rust.
+ * This approach is implemented through generic which can support a large number of branches.
+ * Note: this approach must check every branch, then the execution efficiency is not as good as the
+ * first approach.
+ */
 
-#define MATCH_PARSE_CASE_IMPL1(predict, ...)
-#define MATCH_PARSE_CASE_IMPL(...) MATCH_PARSE_CASE_IMPL1(__VA_ARGS__)
-#define MATCH_PARSE_CASE(T) MATCH_PARSE_CASE_IMPL T
+// template<typename Derived, typename = typename std::enable_if<nstd::is_pure_type<Derived>>::type>
+// class ApplyMatchTrait
+// {
+//     public:
+//     using type = Derived;
+//     operator int() const
+//     {
+//         return static_cast<int>(*static_cast<Derived*>(this));
+//     }
+// };
 
+// template<int I, typename OneType, typename Func>
+// decltype(auto) match_single_branch(OneType&& v, Func&& func)
+// {
 
-#define MATCH_FOR_EACH(c0, ...) MATCH_PARSE_CASE(c0)
+// }
 
-#define MATCH_IMPL(matchable, c0, ...) [&matchable]() -> decaltype(auto) {switch (matchable)
-{
-MATCH_FOR_EACH(c0, __VA_ARGS__)
-default:
-    return {};
-}}
+// template <typename MatchableType, typename ItemFunc, typename Func>
+// decltype(auto) match_impl(MatchableType&& v, ItemFunc&& item_func)
+// {
 
-#define MATCH(matchable, ...) MATCH_IMPL(matchable, __VA_ARGS__)
+// }
 
-MATCH(result, (OK, ||{}), (ERR, ||{}));
+// template <typename MatchableType, typename ItemFunc, typename... ItemFuncs>
+// decltype(auto) match_impl(MatchableType&& v, ItemFunc&& item_func, ItemFuncs&&... item_funcs)
+// {
+//     static_cast<int>(v)
+// }
 
-template<typename Derived, typename = typename std::enable_if<nstd::is_pure_type<Derived>>::type>
-class ApplyMatchTrait
-{
-    public:
-    using type = Derived;
-    operator int() const
-    {
-        return static_cast<int>(*static_cast<Derived*>(this));
-    }
-};
+// template <typename MatchableType, typename... ItemFuncs>
+// decltype(auto) match(MatchableType&& v, ItemFuncs&&... item_funcs)
+// {
+//     return match_impl(v, item_funcs...);
+// }
+// template<typename MatchableType, typename... Funcs>
+// decltype(auto) match(MatchableType* v, Funcs&&... funcs)
+// {
+//     if constexpr (
+//         std::is_same<typename std::remove_cvref<MatchableType>::type, typename
+//         ApplyMatchTrait<std::remove_cvref<MatchableType>::type>>
+//     )
+// }
 
-template<int I, typename OneType, typename Func>
-decltype(auto) match_single_branch(OneType&& v, Func&& func)
-{
-    
-}
-
-
-
-template <typename MatchableType, typename ItemFunc, typename Func>
-decltype(auto) match_impl(MatchableType&& v, ItemFunc&& item_func)
-{
-    
-}
-
-template <typename MatchableType, typename ItemFunc, typename... ItemFuncs>
-decltype(auto) match_impl(MatchableType&& v, ItemFunc&& item_func, ItemFuncs&&... item_funcs)
-{
-    static_cast<int>(v)
-}
-
-template <typename MatchableType, typename... ItemFuncs>
-decltype(auto) match(MatchableType&& v, ItemFuncs&&... item_funcs)
-{
-    return match_impl(v, item_funcs...);
-}
-template<typename MatchableType, typename... Funcs>
-decltype(auto) match(MatchableType* v, Funcs&&... funcs)
-{
-    if constexpr (
-        std::is_same<typename std::remove_cvref<MatchableType>::type, typename ApplyMatchTrait<std::remove_cvref<MatchableType>::type>>
-    )
-}
-}
+} // namespace nstd
 #endif
--- include/result.hpp
+++ include/result.hpp
@@ -2,148 +2,104 @@
 #define __NSTD_RESULT_HPP__
 
 #include <type_traits>
-#include <match.hpp>
 
 namespace nstd {
 
-    enum Status
+enum Status
+{
+    OK,
+    REPORTED_ERR,
+    UNREPORTED_ERR,
+};
+template <typename OkType, typename ErrType>
+class Result //: public ApplyMatchTrait<Result<OkType, ErrType>, Status>
+{
+private:
+    Status m_status;
+    union ResultValue
     {
-        OK,
-        REPORTED_ERR,
-        UNREPORTED_ERR,
-    };
-    template<typename OkType, typename ErrType>
-    class Result: public ApplyMatchTrait<Result<OkType, ErrType>, Status>
+        OkType ok_value;
+        ErrType err_value;
+        ResultValue(OkType&& ok, std::true_type)
+        {
+            new(&ok_value) OkType(std::forward<OkType>(ok));
+        }
+        ResultValue(ErrType&& err, std::false_type)
+        {
+            new(&err_value) ErrType(std::forward<OkType>(err));
+        }
+        ResultValue(const Result<OkType, ErrType>& oe)
+        {
+            switch(oe.m_status)
+            {
+            case Status::OK: new(&ok_value) OkType(oe.m_result.ok_value); break;
+            case Status::REPORTED_ERR:
+            case Status::UNREPORTED_ERR: new(&err_value) ErrType(oe.m_result.err_value); break;
+            default: break;
+            }
+        }
+        ResultValue(Result<OkType, ErrType>&& oe)
+        {
+            switch(oe.m_status)
+            {
+            case Status::OK: new(&ok_value) OkType(std::move(oe.m_result.ok_value)); break;
+            case Status::REPORTED_ERR:
+            case Status::UNREPORTED_ERR:
+                new(&err_value) ErrType(std::move(oe.m_result.err_value));
+                break;
+            default: break;
+            }
+        }
+        ~ResultValue() {}
+    } m_result;
+    Result(OkType&& ok_value) : m_result(std::forward<OkType>(ok_value), std::true_type())
     {
-        private:
-        Status m_status;
-        union ResultValue
-        {
-            OkType ok_value;
-            ErrType err_value;
-            ResultValue(OkType&& ok, std::true_type)
-            {
-                new(&ok_value) OkType(std::forward<OkType>(ok));
-            }
-            ResultValue(ErrType&& err, std::false_type)
-            {
-                new(&err_value) ErrType(std::forward<OkType>(err));
-            }
-            ResultValue(const Result<OkType, ErrType>& oe)
-            {
-                switch (oe.m_status)
-                {
-                case Status::OK:
-                    new(&ok_value) OkType(oe.m_result.ok_value);
-                    break;
-                case Status::REPORTED_ERR:
-                case Status::UNREPORTED_ERR:
-                    new(&err_value) ErrType(oe.m_result.err_value);
-                    break;
-                default:
-                    break;
-                }
-            }
-            ResultValue(Result<OkType, ErrType>&& oe)
-            {
-                switch (oe.m_status)
-                {
-                case Status::OK:
-                    new(&ok_value) OkType(std::move(oe.m_result.ok_value));
-                    break;
-                case Status::REPORTED_ERR:
-                case Status::UNREPORTED_ERR:
-                    new(&err_value) ErrType(std::move(oe.m_result.err_value));
-                    break;
-                default:
-                    break;
-                }
-            }
-            ~ResultValue(){}
-        } m_result;
-        Result(OkType&& ok_value): m_result(std::forward<OkType>(ok_value), std::true_type())
-        {
-            m_status = Status::OK;
-        }
-        Result(Status s, ErrType&& err_value): m_result(std::forward<ErrType>(err_value), std::false_type())
-        {
-            m_status = s;
-        }
-        public:
-        Result(const Result<OkType, ErrType>& res): m_result(res)
-        {
-            m_status = res.m_status;
-        }
-        Result(Result<OkType, ErrType>&& res): m_result(std::move(res))
-        {
-            m_status = res.m_status;
-        }
-        inline Status status()
-        {
-            return m_status;
-        }
-        inline bool is_ok()
-        {
-            return Status::OK == m_status;
-        }
-        inline bool is_err()
-        {
-            return Status::REPORTED_ERR == m_status || Status::UNREPORTED_ERR == m_status;
-        }
-        inline bool is_reported_err()
-        {
-            return Status::REPORTED_ERR == m_status;
-        }
-        inline bool is_unreported_err()
-        {
-            return Status::UNREPORTED_ERR == m_status;
-        }
+        m_status = Status::OK;
+    }
+    Result(Status s, ErrType&& err_value)
+        : m_result(std::forward<ErrType>(err_value), std::false_type())
+    {
+        m_status = s;
+    }
 
-        operator bool()
+public:
+    Result(const Result<OkType, ErrType>& res) : m_result(res) { m_status = res.m_status; }
+    Result(Result<OkType, ErrType>&& res) : m_result(std::move(res)) { m_status = res.m_status; }
+    inline Status status() { return m_status; }
+    inline bool is_ok() { return Status::OK == m_status; }
+    inline bool is_err()
+    {
+        return Status::REPORTED_ERR == m_status || Status::UNREPORTED_ERR == m_status;
+    }
+    inline bool is_reported_err() { return Status::REPORTED_ERR == m_status; }
+    inline bool is_unreported_err() { return Status::UNREPORTED_ERR == m_status; }
+    operator Status() { return m_status; }
+    static Result<OkType, ErrType> ok(OkType&& value)
+    {
+        return Result(std::forward<OkType>(value));
+    }
+    static Result<OkType, ErrType> reported_err(ErrType&& value)
+    {
+        return Result(Status::REPORTED_ERR, std::forward<ErrType>(value));
+    }
+    static Result<OkType, ErrType> unreported_err(ErrType&& value)
+    {
+        return Result(Status::UNREPORTED_ERR, std::forward<ErrType>(value));
+    }
+    ~Result()
+    {
+        // The ~OkType() and ~ErrType() are ok while OkType or ErrType is scalar type.(I use
+        // clang++ 15.0.2 for testing.)
+        switch(m_status)
         {
-            switch (m_status)
-            {
-            case Status::OK:
-                return false;
-            case Status::REPORTED_ERR:
-            case Status::UNREPORTED_ERR:
-                return true;
-            default:
-                break;
-            }
+        case Status::OK: m_result.ok_value.~OkType(); break;
+        case Status::REPORTED_ERR:
+        case Status::UNREPORTED_ERR: m_result.err_value.~ErrType();
+        default: break;
         }
-        operator Status()
-        {
-            return m_status;
-        }
-        static Result<OkType, ErrType> ok(OkType&& value)
-        {
-            return Result(std::forward<OkType>(value));
-        }
-        static Result<OkType, ErrType> reported_err(ErrType&& value)
-        {
-            return Result(Status::REPORTED_ERR, std::forward<ErrType>(value));
-        }
-        static Result<OkType, ErrType> unreported_err(ErrType&& value)
-        {
-            return Result(Status::UNREPORTED_ERR, std::forward<ErrType>(value));
-        }
-        ~Result()
-        {
-            // The ~OkType() and ~ErrType() are ok while OkType or ErrType is scalar type.(I use clang++ 15.0.2 for testing.)
-            switch (m_status)
-            {
-            case Status::OK:
-                m_result.ok_value.~OkType();
-                break;
-            case Status::REPORTED_ERR:
-            case Status::UNREPORTED_ERR:
-                m_result.err_value.~ErrType();
-            default:
-                break;
-            }
-        }
-    };
-}
+    }
+};
+
+} // namespace nstd
 
 #endif
