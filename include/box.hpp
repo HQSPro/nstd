422131fc65ce9a3e4fea166ec2f4da8f12c1ac71
wenjh <zpwenjh@163.com>
2023-07-20 10:33:18 +0800

    Match multiple cases in one branch.

--- include/box.hpp
+++
@@ -1,5 +0,0 @@
-
-#ifndef __NSTD_BOX_HPP__
-#define __NSTD_BOX_HPP__
-
-#endif
--- include/match.hpp
+++ include/match.hpp
@@ -4,7 +4,6 @@
 
 #include <assert.h>
 #include <type_magic.hpp>
-#include <result.hpp>
 
 namespace nstd {
 
@@ -13,37 +12,9 @@ namespace nstd {
  * Note: currently, only 30 branches are supported for matching and we don't support match gurd.
  */
 
-template <int N, typename T>
-struct MatchGet
-{
-    static decltype(auto) match_get(T&& t, nstd::Int<N> n) { return t.get(n); }
-}
-
-template <int N>
-struct MatchGet<N, int>
-{
-    static decltype(auto) match_get(const int& t, nstd::Int<N>)
-    {
-        assert(N == t);
-        return t;
-    }
-}
-
 #define MATCH_PP_EMPTY
 #define MATCH_CONCAT_IMPL(A, B) A##B
 #define MATCH_CONCAT(A, B) MATCH_CONCAT_IMPL(A, B)
-
-#define MATCH_DEFAULT_SYMBOL_ _, 1
-#define MATCH_CASE_OR_DEFAULT_IMPL_0(m, c, ...)                                                \
-    case c:                                                                                    \
-        return __VA_ARGS__(MatchGet<static_cast<int>(c), pure_type_t<decltype(m)>>::match_get( \
-            std::move(m), nstd::Int<static_cast<int>(c)>{}));
-#define MATCH_CASE_OR_DEFAULT_CONDITION(a, b, ...) b
-#define MATCH_CASE_OR_DEFAULT_IMPL_1(m, c, ...) \
-    default:                                    \
-        return __VA_ARGS__();
-#define MATCH_CASE_OR_DEFAULT_CHECK(...) MATCH_CASE_OR_DEFAULT_CONDITION(__VA_ARGS__, 0)
-#define MATCH_CASE_OR_DEFAULT_IMPL(condition) MATCH_CONCAT(MATCH_CASE_OR_DEFAULT_IMPL_, condition)
 
 #define MATCH_PARENS_PROBE(...) _, 1
 #define MATCH_PARENS_CONDITION(a, b, ...) b
@@ -55,17 +26,129 @@ struct MatchGet<N, int>
 #define MATCH_PARENS_REMOVE(expr) \
     MATCH_PARENS_REMOVE_IMPL(MATCH_PARENS_CHECK(MATCH_PARENS_PROBE expr))(expr)
 
-#define MATCH_CASE_PROBE(...) _, 1
-#define MATCH_CASE_CONDITION(a, b, ...) b
-#define MATCH_CASE_CHECK(...) MATCH_CASE_CONDITION(__VA_ARGS__, 0)
+#define MATCH_DEFAULT_SYMBOL_ _, 1
+#define MATCH_CASE_OR_DEFAULT_CONDITION(a, b, ...) b
+#define MATCH_CASE_OR_DEFAULT_CHECK(...) MATCH_CASE_OR_DEFAULT_CONDITION(__VA_ARGS__, 0)
+#define MATCH_CASE_GEN_IMPL_0(c) case c:
+#define MATCH_CASE_GEN_IMPL_1(c) MATCH_PP_EMPTY
+#define MATCH_CASE_GEN_IMPL(condition) MATCH_CONCAT(MATCH_CASE_GEN_IMPL_, condition)
+#define MATCH_CASE_GEN(c) \
+    MATCH_CASE_GEN_IMPL(MATCH_CASE_OR_DEFAULT_CHECK(MATCH_CONCAT(MATCH_DEFAULT_SYMBOL, c)))(c)
+#define MATCH_CASE_EXPAND_IMPL(c0,  \
+                               c1,  \
+                               c2,  \
+                               c3,  \
+                               c4,  \
+                               c5,  \
+                               c6,  \
+                               c7,  \
+                               c8,  \
+                               c9,  \
+                               c10, \
+                               c11, \
+                               c12, \
+                               c13, \
+                               c14, \
+                               c15, \
+                               c16, \
+                               c17, \
+                               c18, \
+                               c19, \
+                               c20, \
+                               c21, \
+                               c22, \
+                               c23, \
+                               c24, \
+                               c25, \
+                               c26, \
+                               c27, \
+                               c28, \
+                               c29, \
+                               ...) \
+    MATCH_CASE_GEN(c0)              \
+    MATCH_CASE_GEN(c1)              \
+    MATCH_CASE_GEN(c2)              \
+    MATCH_CASE_GEN(c3)              \
+    MATCH_CASE_GEN(c4)              \
+    MATCH_CASE_GEN(c5)              \
+    MATCH_CASE_GEN(c6)              \
+    MATCH_CASE_GEN(c7)              \
+    MATCH_CASE_GEN(c8)              \
+    MATCH_CASE_GEN(c9)              \
+    MATCH_CASE_GEN(c10)             \
+    MATCH_CASE_GEN(c11)             \
+    MATCH_CASE_GEN(c12)             \
+    MATCH_CASE_GEN(c13)             \
+    MATCH_CASE_GEN(c14)             \
+    MATCH_CASE_GEN(c15)             \
+    MATCH_CASE_GEN(c16)             \
+    MATCH_CASE_GEN(c17)             \
+    MATCH_CASE_GEN(c18)             \
+    MATCH_CASE_GEN(c19)             \
+    MATCH_CASE_GEN(c20)             \
+    MATCH_CASE_GEN(c21)             \
+    MATCH_CASE_GEN(c22)             \
+    MATCH_CASE_GEN(c23)             \
+    MATCH_CASE_GEN(c24)             \
+    MATCH_CASE_GEN(c25)             \
+    MATCH_CASE_GEN(c26)             \
+    MATCH_CASE_GEN(c27)             \
+    MATCH_CASE_GEN(c28)             \
+    MATCH_CASE_GEN(c29)
+
+#define MATCH_CASE_EXPAND(...)          \
+    MATCH_CASE_EXPAND_IMPL(__VA_ARGS__, \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _,           \
+                           _)
+
+#define MATCH_CASE_OR_DEFAULT_IMPL_0(m, c, ...) \
+    MATCH_CASE_EXPAND(MATCH_PARENS_REMOVE(c))   \
+    return __VA_ARGS__();
+
+#define MATCH_CASE_OR_DEFAULT_IMPL_1(m, c, ...) \
+    default:                                    \
+        return __VA_ARGS__();
+
+#define MATCH_CASE_OR_DEFAULT_IMPL(condition) MATCH_CONCAT(MATCH_CASE_OR_DEFAULT_IMPL_, condition)
+
+#define MATCH_CASE_FIRST_IMPL(c, ...) c
+#define MATCH_CASE_FIRST(...) __VA_ARGS__
 #define MATCH_CASE_PARSE_IMPL_0(...) MATCH_PP_EMPTY
-#define MATCH_CASE_PARSE_IMPL_1_IMPL(m, c, ...)                                                    \
-    MATCH_CASE_OR_DEFAULT_IMPL(MATCH_CASE_OR_DEFAULT_CHECK(MATCH_CONCAT(MATCH_DEFAULT_SYMBOL, c))) \
+#define MATCH_CASE_PARSE_IMPL_1_IMPL(m, c, ...)                                        \
+    MATCH_CASE_OR_DEFAULT_IMPL(MATCH_CASE_OR_DEFAULT_CHECK(                            \
+        MATCH_CONCAT(MATCH_DEFAULT_SYMBOL, MATCH_CASE_FIRST(MATCH_PARENS_REMOVE(c))))) \
     (m, c, __VA_ARGS__)
 #define MATCH_CASE_PARSE_IMPL_1(m, c) MATCH_CASE_PARSE_IMPL_1_IMPL(m, c)
 #define MATCH_CASE_PARSE_IMPL(condition) MATCH_CONCAT(MATCH_CASE_PARSE_IMPL_, condition)
 #define MATCH_CASE_PARSE(m, c) \
-    MATCH_CASE_PARSE_IMPL(MATCH_CASE_CHECK(MATCH_CASE_PROBE c))(m, MATCH_PARENS_REMOVE(c))
+    MATCH_CASE_PARSE_IMPL(MATCH_PARENS_CHECK(MATCH_PARENS_PROBE c))(m, MATCH_PARENS_REMOVE(c))
 
 #define MATCH_CASES_PARSE_IMPL(m,   \
                                c0,  \
